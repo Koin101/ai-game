42cd2051c72e7bed82bbb41b67bfc00de4d172b1
Koin101 <45605528+Koin101@users.noreply.github.com>
2024-09-17 14:03:12 +0200

    Basic game with sprites

---
+++ infinite-realms/Scripts/MainCharacter.cs
@@ -0,0 +1,73 @@
+using Godot;
+using System;
+
+public partial class MainCharacter : CharacterBody2D
+{
+	public const float Speed = 150.0f;
+	public const float JumpVelocity = -320.0f;
+
+	private AnimatedSprite2D _animatedSprite;
+	private bool _isJumping = false;
+	public override void _Ready()
+	{
+		_animatedSprite = GetNode<AnimatedSprite2D>("MainCharAnimation");
+	}
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("Jump") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+			_isJumping = true;
+			_animatedSprite.Play("Jumping");
+		}
+		else if(IsOnFloor()) _isJumping = false;
+
+
+
+		Vector2 direction = Input.GetVector("Move_Left", "Move_Right",
+			"ui_up", "ui_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.X = direction.X * Speed;
+			if (!_isJumping)
+			{
+				_animatedSprite.FlipH = direction.X switch
+				{
+					< 0 => true,
+					> 0 => false,
+					_ => _animatedSprite.FlipH
+				};
+				_animatedSprite.Play("Running");
+			}
+			else
+			{
+				_animatedSprite.FlipH = direction.X switch
+				{
+					< 0 => true,
+					> 0 => false,
+					_ => _animatedSprite.FlipH
+				};
+			}
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			if(!_isJumping)
+				_animatedSprite.Play("Idle");
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
