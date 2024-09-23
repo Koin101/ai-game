0de603386c40c87c4c5a4f4e8c6e01eee04166c3
Koin101 <45605528+Koin101@users.noreply.github.com>
2024-09-23 10:50:56 +0200

    generating templates working

--- infinite-realms/Scripts/level_1.gd
+++ infinite-realms/Scripts/level_1.gd
@@ -1,77 +1,27 @@
 extends Node2D
 
 signal rendered
+signal can_climb
 
-func render(map : TileMap):
-
-	var parent = self.get_parent()
-	print("Rendering : ", parent.name)
-
-	await RenderingServer.frame_post_draw
-	await get_tree().create_timer(1).timeout
-
-	var cell_size = map.tile_set.tile_size
-	var map_tile_size = map.get_used_rect()
+func get_bounds() -> Rect2i :
+	var cell_size = $TileMap.tile_set.tile_size
+	var map_tile_size = $TileMap.get_used_rect()
 	var vp = get_viewport().get_visible_rect().size
 	var map_pix_width = map_tile_size.size.x * cell_size.x
 	var map_pix_height = map_tile_size.size.y * cell_size.y
-	var map_pix_offset_x = map_tile_size.position.x * cell_size.x
-	var map_pix_offset_y = map_tile_size.position.y * cell_size.y
+	return Rect2i(map_tile_size.position.x, map_tile_size.position.y, map_pix_width, map_pix_height)
 
-	print("cell size : ", cell_size)
-	print("map tile size : ", map_tile_size)
-	print("viewport size : ", vp)
-	print("map pixel dimensions : (", map_pix_width, ", ", map_pix_height, ")")
-	print("map pixel offset : (", map_pix_offset_x , ", ", map_pix_offset_y, ")")
-	print("Screen size : ", DisplayServer.screen_get_size())
+func toggleBackground(state : bool):
+	$ColorRect.visible = state
 
-	# find the biggest extent
-	var vp_max_x = get_viewport().get_visible_rect().size.x
-	var vp_max_y = get_viewport().get_visible_rect().size.y
-	var map_max_x = map_pix_offset_x + map_pix_width
-	var map_max_y = map_pix_offset_y + map_pix_height
-	
-	var max_x = max(vp_max_x, map_max_x)
-	var max_y = max(vp_max_y, map_max_y)
+func render():
+	toggleBackground(true)
+	$RenderScene.render($PlatformLayer)
 
-	print("final map size : (", max_x, ", " , max_y, ")")
-	var format = get_viewport().get_texture().get_image().get_format()
-	var img = Image.create(max_x, max_y, false, format)
+func _ready():
+	print("TileMapLevel1 loaded")
+	$RenderScene.rendered.connect(_on_render_complete)
+	pass
 
-	var current_y = 0
-	var current_x = 0
-	var sourceRect = get_viewport().get_visible_rect()
-
-	var moreY = true
-	var yStep = int(vp_max_y  / 5)
-	var xStep = int(vp_max_x / 5)
-
-	while moreY:
-		map.position.y = current_y
-		current_x = 0
-		#print("map y = ", map.position.y)
-		while current_x < max_x:
-			map.position.x = -current_x
-			#print("map x = ", map.position.x)
-			map.can_process()
-			await RenderingServer.frame_post_draw
-			await get_tree().create_timer(0.05).timeout
-			var chunk = get_viewport().get_texture().get_image()
-			var destPos = Vector2i(current_x, abs(current_y))
-			img.blit_rect(chunk, sourceRect, destPos)
-			current_x = current_x + xStep
-
-		current_y = current_y - yStep
-		if current_y < -max_y - yStep:
-			moreY = false
-
-	var template = "template-" +  parent.name + ".png"
-	var dir = DirAccess.open(".")
-	if dir:
-		if not dir.dir_exists("templates"):
-			dir.make_dir("templates")
-
-	img.save_png("./templates/" + template)
-	print("render saved to : ", template)
-	await RenderingServer.frame_post_draw
+func _on_render_complete():
 	rendered.emit()
