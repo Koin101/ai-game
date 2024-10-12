f38e419d730fd2157397d0ec89be2de5aca36dac
Koin101 <45605528+Koin101@users.noreply.github.com>
2024-10-12 18:13:02 +0200

    simple menu

---
+++ infinite-realms/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GetNode<Button>("VBoxContainer/StartButton").GrabFocus();
+	}
+
+	private void _on_start_button_pressed()
+	{
+		GetTree().ChangeSceneToFile("res://Scenes/Levels/Level1.tscn");
+	}
+	
+	private void _on_options_button_pressed()
+	{
+		
+	}
+	
+	private void _on_exit_button_pressed()
+	{
+		GetTree().Quit();
+	}
+}
