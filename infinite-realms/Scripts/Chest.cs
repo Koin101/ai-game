2b2db33b4fc179c2c4de973728800f8646f1dbda
Koin101 <45605528+Koin101@users.noreply.github.com>
2024-10-18 15:16:36 +0200

    chest improved

--- infinite-realms/Scripts/Chest.cs
+++ infinite-realms/Scripts/Chest.cs
@@ -5,8 +5,8 @@ public partial class Chest : Node2D
 {
     public bool opened = false;
     public bool inRange = false;
-    
-    
+    public MainCharacter player;
+    public Area2D chestArea;
     public void _on_chest_area_body_entered(MainCharacter body)
     {
         GD.Print("Chest entered");
@@ -18,13 +18,21 @@ public partial class Chest : Node2D
         GD.Print("Chest exited");
         inRange = false;
     }
-    
+
+    public override void _Ready()
+    {
+        base._Ready();
+        player = GetTree().GetFirstNodeInGroup("Player") as MainCharacter;
+        chestArea = GetNode<Area2D>("ChestArea");
+    }
+
     public override void _Process(double delta)
     {
         base._Process(delta);
-        if(!opened && inRange && Input.IsActionPressed("chat"))
+
+        if (chestArea.OverlapsBody(player) && Input.IsActionPressed("chat") && !opened)
         {
-            GD.Print("Chest opened");
+            GD.Print("chest opened");
             opened = true;
             GetNode<AnimatedSprite2D>("OpenCloseAnimation").Frame = 1;
         }
