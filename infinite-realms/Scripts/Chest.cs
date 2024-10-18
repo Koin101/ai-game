c4694c555a62d81eed9203afeeea064c1a906564
rubenvdz <71780895+rubenvdz@users.noreply.github.com>
2024-10-18 18:21:38 +0200

    Chest key indicator
    Net als bij NPC en portal

--- infinite-realms/Scripts/Chest.cs
+++ infinite-realms/Scripts/Chest.cs
@@ -3,38 +3,46 @@ using System;
 
 public partial class Chest : Node2D
 {
-    public bool opened = false;
-    public bool inRange = false;
-    public MainCharacter player;
-    public Area2D chestArea;
-    public void _on_chest_area_body_entered(MainCharacter body)
-    {
-        GD.Print("Chest entered");
-        inRange = true;
-    }
-    
-    public void _on_chest_area_body_exited(MainCharacter body)
-    {
-        GD.Print("Chest exited");
-        inRange = false;
-    }
+	public bool opened = false;
+	public bool inRange = false;
+	public MainCharacter player;
+	public Area2D chestArea;
+	public Sprite2D keyIndicator;
+	public void _on_chest_area_body_entered(MainCharacter body)
+	{
+		GD.Print("Chest entered");
+		inRange = true;
+		if (!opened) {
+			keyIndicator.Visible = true;
+		}
+	}
+	
+	public void _on_chest_area_body_exited(MainCharacter body)
+	{
+		GD.Print("Chest exited");
+		inRange = false;
+		keyIndicator.Visible = false;
+	}
 
-    public override void _Ready()
-    {
-        base._Ready();
-        player = GetTree().GetFirstNodeInGroup("Player") as MainCharacter;
-        chestArea = GetNode<Area2D>("ChestArea");
-    }
+	public override void _Ready()
+	{
+		base._Ready();
+		player = GetTree().GetFirstNodeInGroup("Player") as MainCharacter;
+		chestArea = GetNode<Area2D>("ChestArea");
+		keyIndicator = GetNode<Sprite2D>("KeyIndicator");
+		keyIndicator.Visible = false;
+	}
 
-    public override void _Process(double delta)
-    {
-        base._Process(delta);
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
 
-        if (chestArea.OverlapsBody(player) && Input.IsActionPressed("chat") && !opened)
-        {
-            GD.Print("chest opened");
-            opened = true;
-            GetNode<AnimatedSprite2D>("OpenCloseAnimation").Frame = 1;
-        }
-    }
+		if (chestArea.OverlapsBody(player) && Input.IsActionPressed("chat") && !opened)
+		{
+			GD.Print("chest opened");
+			opened = true;
+			keyIndicator.Visible = false;
+			GetNode<AnimatedSprite2D>("OpenCloseAnimation").Frame = 1;
+		}
+	}
 }
